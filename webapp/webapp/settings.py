271f3830be4e0d34c312d58c3668ee5d1b7ca670
aadish <aadishgupta7791@gmail.com>
2017-04-13 10:59:04 -0600

    rendering HTML from DJANGO

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +116,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+#TEMPLATES_DIR = (
+#    os.path.join(SETTINGS_PATH, 'templates'),
+#)
